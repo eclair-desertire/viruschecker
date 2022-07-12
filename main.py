7d6e2b09020054990f8d9c4596b1886c4557be74
Alisher Ruziyev <eclair.desertire@gmail.com>
2022-07-12 15:59:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -15,27 +15,37 @@ class App:
 class Window(Tk.tk):
     def __init__(self, loop):
         self.loop = loop
+        self.client=vt.Client(apikey.api_key)
         self.root = tk.Tk()
-        self.label = tk.Label(text="")
-        self.label.grid(row=0, columnspan=2, padx=(8, 8), pady=(16, 0))
-        self.progressbar = ttk.Progressbar(length=280)
-        self.progressbar.grid(row=1, columnspan=2, padx=(8, 8), pady=(16, 0))
-        button_block = tk.Button(text="Calculate Sync", width=10, command=self.calculate_sync)
-        button_block.grid(row=2, column=0, sticky=tk.W, padx=8, pady=8)
-        button_non_block = tk.Button(text="Calculate Async", width=10, command=lambda: self.loop.create_task(self.calculate_async()))
-        button_non_block.grid(row=2, column=1, sticky=tk.W, padx=8, pady=8)
+        self.root.geometry("500x300")
+        self.root.geometry("500x300")
+        self.root.resizable(0,0)
+        self.root.title("Viruschecker")
+        self.root.configure(bg="white")
+        self.vir_url=StringVar()
+        Label(self.root,text="Paste the Link you want to check here:",font="arial 15 bold",
+        bg="white").place(x=20,y=20)
+        self.link_enter=Entry(self.root, width=50, textvariable=self.vir_url).place(x=20,y=40)
+        Button(self.root,text="Check url",command=self.scan_link).place(x=190,y=70)
+        Button(self.root, text="Select File to check",command=self.scan_file).place(x=20,y=200)
 
     async def show(self):
         while True:
-            self.label["text"] = self.animation
-            self.animation = self.animation[1:] + self.animation[0]
             self.root.update()
             await asyncio.sleep(.1)
 
-    def calculate_sync(self):
-        max = 3000000
-        for i in range(1, max):
-            self.progressbar["value"] = i / max * 100
+    async def scan_file(self):
+        file=filedialog.askopenfile()
+        await asyncio.sleep(0)
+
+    async def scan_link(self):
+        self.client.scan_url(self.vir_url.get())
+        analysis = vt.url_id(self.vir_url.get())
+        url = self.client.get_object("/urls/{}".format(analysis))
+
+        print(url.times_submitted)
+        print(url.last_analysis_stats)
+        await asyncio.sleep(0)
 
     async def calculate_async(self):
         max = 3000000
@@ -46,31 +56,14 @@ class Window(Tk.tk):
 
 asyncio.run(App().exec())
 
-root=Tk()
-root.geometry("500x300")
-root.resizable(0,0)
-root.title("Viruschecker")
-root.configure(bg="white")
 
-vir_url=StringVar()
-Label(root,text="Paste the Link you want to check here:",font="arial 15 bold",
-    bg="white").place(x=20,y=20)
-link_enter=Entry(root, width=50, textvariable=vir_url).place(x=20,y=40)
+
+
+
+
 
 #root.filename=filedialog.askopenfilename(initialdir="/",title="Select a file",filetypes=(("all files","*.*")))
-def scan_file():
-    file=filedialog.askopenfile()
 
-def scan_link():
-    client.scan_url(vir_url.get())
-    analysis = vt.url_id(vir_url.get())
-    url = client.get_object("/urls/{}".format(analysis))
 
-    print(url.times_submitted)
-    print(url.last_analysis_stats)
 
-Button(root,text="Check url",command=threading.Thread(target=scan_link).start).place(x=190,y=70)
-Button(root, text="Select File to check",command=threading.Thread(target=scan_file).start).place(x=20,y=200)
-client=vt.Client(apikey.api_key)
 
-root.mainloop()
