95515f9ccbb2a033408c6df7b3397d32df62b58e
Alisher Ruziyev <eclair.desertire@gmail.com>
2022-07-12 15:00:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import pdb
 import apikey # your own file with YOUR API KEY, you can get it on virustotal site
 import vt
 from tkinter import *
 import threading
 
 
-pdb.set_trace()
 
 root=Tk()
 root.geometry("500x300")
 root.resizable(0,0)
 root.title("Viruschecker")
-root.configure(bg="black")
+root.configure(bg="white")
+
+vir_url=StringVar()
+Label(root,text="Paste the Link you want to check here:",font="arial 15 bold",
+    bg="white").place(x=40,y=40)
+link_enter=Entry(root, width=50, textvariable=vir_url).place(x=32,y=90)
 
 def scan_file():
     pass
