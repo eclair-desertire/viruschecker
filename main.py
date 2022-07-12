162ccb2b70a8024f08b0f0cd1b03e047cf02cb70
Alisher Ruziyev <eclair.desertire@gmail.com>
2022-07-12 14:51:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import apikey # your own file with YOUR API KEY, you can get it on virustotal si
 import vt
 from tkinter import *
 
+
+def scan_file():
+    pass
+
+def scan_url():
+    pass
+
 client=vt.Client(apikey.api_key)
