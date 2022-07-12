3a9e9c2a9348d4fc74b32f2d4c77dc97a0506a0e
Alisher Ruziyev <eclair.desertire@gmail.com>
2022-07-12 14:49:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import apikey
+from http import client
+import apikey # your own file with YOUR API KEY, you can get it on virustotal site
 import vt
+from tkinter import *
 
+client=vt.Client(apikey.api_key)
