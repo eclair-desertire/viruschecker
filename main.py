00e109018abc3b4325c39eb5d1ba8aa5aa1aa67c
Alisher Ruziyev <eclair.desertire@gmail.com>
2022-07-12 14:56:10 +0600

    init tkinter

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from http import client
+import pdb
 import apikey # your own file with YOUR API KEY, you can get it on virustotal site
 import vt
 from tkinter import *
+import threading
 
+
+pdb.set_trace()
+
+root=Tk()
+root.geometry("500x300")
+root.resizable(0,0)
+root.title("Viruschecker")
+root.configure(bg="black")
 
 def scan_file():
     pass
@@ -11,3 +20,5 @@ def scan_url():
     pass
 
 client=vt.Client(apikey.api_key)
+
+root.mainloop()
