834be320648851c0fd1f0e694e2d1d31505d56e9
Alisher Ruziyev <eclair.desertire@gmail.com>
2022-07-19 14:05:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,13 @@ class Window(tk.Tk):
         self.link_enter=Entry(self.root, width=50, textvariable=self.vir_url).place(x=20,y=40)
         Button(self.root,text="Check url",command=self.scan_link).place(x=190,y=70)
         Button(self.root, text="Select File to check",command=self.scan_file).place(x=20,y=200)
-
+    
+    async def
+    
     async def show(self):
         while True:
             self.root.update()
-            await asyncio.sleep(.1)
+            await asyncio.sleep(0.1)
 
     async def scan_file(self):
         file=filedialog.askopenfile()
@@ -43,17 +45,10 @@ class Window(tk.Tk):
         analysis = vt.url_id(self.vir_url.get())
         url = self.client.get_object("/urls/{}".format(analysis))
 
-        print(url.times_submitted)
+
         messagebox.showinfo("Information about link",f"Times submitted:{url.times_submitted}")
-        print(url.last_analysis_stats)
         await asyncio.sleep(0)
 
-    async def calculate_async(self):
-        max = 3000000
-        for i in range(1, max):
-            self.progressbar["value"] = i / max * 100
-            if i % 1000 == 0:
-                await asyncio.sleep(0)
 
 asyncio.run(App().exec())
 
